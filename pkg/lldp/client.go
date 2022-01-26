15daa46f1db76940aff42ffb979f4911eeaadb74
Stefan Majer <stefan.majer@gmail.com>
2022-01-26 09:57:14 +0100

    Move lldp client impl from metal-core (#15)

---
+++ pkg/lldp/client.go
@@ -0,0 +1,86 @@
+package lldp
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+)
+
+// Client consumes lldp messages.
+type Client struct {
+	source *gopacket.PacketSource
+	handle *pcap.Handle
+	ctx    context.Context
+}
+
+// DiscoveryResult holds optional TLV SysName and SysDescription fields of a real lldp frame.
+type DiscoveryResult struct {
+	SysName        string
+	SysDescription string
+}
+
+// NewClient creates a new lldp client.
+func NewClient(ctx context.Context, iface net.Interface) (*Client, error) {
+	handle, err := pcap.OpenLive(iface.Name, 65536, true, 5*time.Second)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open interface:%s in promiscuous mode: %w", iface.Name, err)
+	}
+
+	// filter only lldp packages
+	bpfFilter := fmt.Sprintf("ether proto %#x", etherType)
+	err = handle.SetBPFFilter(bpfFilter)
+	if err != nil {
+		return nil, fmt.Errorf("unable to filter lldp ethernet traffic %#x on interface:%s %w", etherType, iface.Name, err)
+	}
+
+	src := gopacket.NewPacketSource(handle, handle.LinkType())
+	return &Client{
+		source: src,
+		handle: handle,
+		ctx:    ctx,
+	}, nil
+}
+
+// Start searches on the configured interface for lldp packages and
+// pushes the optional TLV SysName and SysDescription fields of each
+// found lldp package into the given channel.
+func (l *Client) Start(resultChan chan<- DiscoveryResult) {
+	defer func() {
+		close(resultChan)
+		l.Close()
+	}()
+
+	for {
+		select {
+		default:
+			for packet := range l.source.Packets() {
+				if packet.LinkLayer().LayerType() != layers.LayerTypeEthernet {
+					continue
+				}
+				for _, layer := range packet.Layers() {
+					if layer.LayerType() != layers.LayerTypeLinkLayerDiscoveryInfo {
+						continue
+					}
+					info := layer.(*layers.LinkLayerDiscoveryInfo)
+					dr := DiscoveryResult{
+						SysName:        info.SysName,
+						SysDescription: info.SysDescription,
+					}
+					resultChan <- dr
+				}
+			}
+		case <-l.ctx.Done():
+			return
+		}
+	}
+}
+
+// Close the LLDP client
+func (l *Client) Close() {
+	l.handle.Close()
+}
